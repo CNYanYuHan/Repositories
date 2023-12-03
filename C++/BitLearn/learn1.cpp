8a447a69b055a9bf529850b9cefa59ceb1391e04
YanYuhan <3304954463@qq.com>
2023-12-03 20:54:26 +0800

    all

---
+++ C++/BitLearn/learn1.cpp
@@ -0,0 +1,28 @@
+/*数据类型
+char 字符数据类型（a b c d）
+int 整型
+short 短整型(12)
+long 长整型
+long long 更长的整型
+float 单精度浮点数
+double 双精度浮点数
+*
+'a'-字符a
+
+
+*/
+int main()
+{
+    //字符类型
+    char ch = 'a';
+    //整型
+    int age =20;
+    //短整形
+    short num =10;
+    //单精度浮点型
+    float weight =55.5;
+    //双精度浮点型
+    double d = 0.00005;
+
+    return 0;
+}
