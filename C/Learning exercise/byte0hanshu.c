df299891bfdc2902640ea790d1c268c852ef2ae5
CNYanYuHan <3304954463@qq.com>
2023-12-14 10:46:30 +0800

    666666666

--- C/Learning exercise/byte0hanshu.c
+++ C/Learning exercise/byte0hanshu.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-int main()
-{
-    int line = 0;
-    while(line < 100)
-    {
-        printf("好好写代码%d\n",line);
-        line++;
-    }
-    if(line == 100)
-    {
-        printf("恭喜您获得好OFFER");
-    }
-
-    return 0;
+int main() {
+  int line = 0;
+  while (line < 100) {
+    printf("好好写代码%d\n", line);
+    line++;
+  }
+  if (line == 100) {
+    printf("恭喜您获得好OFFER");
+  }
+  // 846548646
+  return 0;
 }
