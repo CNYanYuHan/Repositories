3f4ad00f78179ca6fa07918a1431d7e4ea6bfaa2
CNYanYuHan <3304954463@qq.com>
2024-01-28 18:09:26 +0800

    666

--- C/text/test.c
+++ C/text/test.c
@@ -1,23 +1,25 @@
-
 #include <stdio.h>
-void fun(char *s) {
-  int i, j = 0;
-  for (i = 0; s[i] != '\0'; i++){
-    if (s[i] != 'c')
-    /********found********/
-    {
-      s[j] = s[i];
-      j++;
-    }
-    else{
-      continue;
-    }
+#define N 4
+void fun(int (*t)[N], int m) {
+  int i, j;
+  /**********found**********/
+  for (i = 0; i < N; i++) {
+    for (j = N - 1 - m; j >= 0; j--)
+      /**********found**********/
+      t[i][j + 1] = t[i][j];
+    /**********found**********/
+    for (j = 0; j < m; j++)
+      t[i][j] = 0;
   }
-  /********found********/
 }
 void main() {
-  char s[80];
-  gets(s);
-  fun(s);
-  puts(s);
+  int t[][N] = {21, 12, 13, 24, 25, 16, 47, 38, 29, 11, 32, 54, 42, 21, 33, 10},
+      i, j, m;
+  scanf("%d", &m);
+  fun(t, m);
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < N; j++)
+      printf("%3d", t[i][j]);
+    printf("\n");
+  }
 }
--- C/text/yyh.c
+++ C/text/yyh.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-
-int main()
-{
-    int line = 0;
-    while (line < 30)
-    {
-        printf("写代码：%d\n", line);
-        line++;
-    }
-    if (line == 30)
-    {
-        printf("好offer");
-    }
-    return 0;
+#include <string.h>
+void fun(char *s, char *t) {
+  int i, s1;
+  s1 = strlen(s);
+  /********found********/
+  for (i = 0; i < s1; i++)
+    t[i] = s[i];
+  for (i = 0; i < s1; i++)
+    t[s1 + i] = s[s1 - i - 1];
+  /********found********/
+  t[2*s1] = '\0';
+}
+void main() {
+  char s[100], t[100];
+  scanf("%s",s);
+  fun(s,t);
+  printf("\n%s", t);
 }
